29e78f06d808a77a59474e0e117acf4b7b3e7da2
YerrapothuPavan <77278346+YerrapothuPavan@users.noreply.github.com>
2021-02-04 15:59:19 +0530

    adding new file for build failure

---
+++ src/main/java/com/example/demo/processor/MathService.java
@@ -0,0 +1,12 @@
+package com.example.demo.processor;
+
+public class MathService {
+    public int multiply(int firstNumber, int secondNumber) {
+
+        return firstNumber * secondNumber;
+    }
+
+    public int subtract(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+}
